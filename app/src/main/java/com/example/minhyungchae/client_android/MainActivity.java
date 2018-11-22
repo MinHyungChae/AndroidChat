2343d02e2af80891181fa743381d1a8be9aa401c
MinhyungChae <fkdlskmin1@gmail.com>
2018-11-23 00:45:44 +0900

    music play/pause ok//couldn't stream on both phone

--- app/src/main/java/com/example/minhyungchae/client_android/MainActivity.java
+++ app/src/main/java/com/example/minhyungchae/client_android/MainActivity.java
@@ -5,10 +5,16 @@ import android.app.AlertDialog;
 import android.app.ProgressDialog;
 import android.drm.DrmStore;
 import android.graphics.Color;
+import android.media.AudioManager;
+import android.media.MediaPlayer;
+import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.util.TypedValue;
+import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -20,9 +26,11 @@ import org.w3c.dom.Text;
 
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.Socket;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -40,15 +48,166 @@ public class MainActivity extends AppCompatActivity {
     // 사용자 닉네임( 내 닉넴과 일치하면 내가보낸 말풍선으로 설정 아니면 반대설정)
     String user_nickname;
 
+    // streaming 관련
+    EditText edit_url;
+    Button btn_upload;
+    Button btn_list;
+    Button btn_mute;
+
+    private boolean playPause;
+    private MediaPlayer mediaPlayer;
+    private boolean intialStage = true;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //chatting 관련
         edit1 = findViewById(R.id.editText);
         btn1 = findViewById(R.id.button);
         container=findViewById(R.id.container);
         scroll=findViewById(R.id.scroll);
+
+        //streaming 관련
+        edit_url = findViewById(R.id.musicURL);
+        btn_upload = findViewById(R.id.musicUpload);
+        btn_list = findViewById(R.id.muslicList);
+        btn_mute = findViewById(R.id.musicPause);
+
+        mediaPlayer = new MediaPlayer();
+        mediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
+        btn_upload.setOnClickListener(pausePlay);
+
     }
+
+    private View.OnClickListener pausePlay = new View.OnClickListener() {
+
+        @Override
+        public void onClick(View v) {
+            // TODO Auto-generated method stub
+            // TODO Auto-generated method stub
+
+            if (!playPause) {
+                btn_upload.setText("play1");
+                if (intialStage)
+                    new Player().execute("http://www.hrupin.com/wp-content/uploads/mp3/testsong_20_sec.mp3");
+                else {
+                    if (!mediaPlayer.isPlaying())
+                        mediaPlayer.start();
+                }
+                playPause = true;
+            } else {
+                btn_upload.setText("pause1");
+                if (mediaPlayer.isPlaying())
+                    mediaPlayer.pause();
+                playPause = false;
+            }
+        }
+    };
+
+    class Player extends AsyncTask<String, Void, Boolean> {
+        private ProgressDialog progress;
+
+        @Override
+        protected Boolean doInBackground(String... params) {
+            // TODO Auto-generated method stub
+            Boolean prepared;
+            try {
+
+                mediaPlayer.setDataSource(params[0]);
+
+                mediaPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+
+                    @Override
+                    public void onCompletion(MediaPlayer mp) {
+                        // TODO Auto-generated method stub
+                        intialStage = true;
+                        playPause=false;
+                        btn_upload.setText("play2");
+                        mediaPlayer.stop();
+                        mediaPlayer.reset();
+                    }
+                });
+                mediaPlayer.prepare();
+                prepared = true;
+            } catch (IllegalArgumentException e) {
+                // TODO Auto-generated catch block
+                Log.d("IllegarArgument", e.getMessage());
+                prepared = false;
+                e.printStackTrace();
+            } catch (SecurityException e) {
+                // TODO Auto-generated catch block
+                prepared = false;
+                e.printStackTrace();
+            } catch (IllegalStateException e) {
+                // TODO Auto-generated catch block
+                prepared = false;
+                e.printStackTrace();
+            } catch (IOException e) {
+                // TODO Auto-generated catch block
+                prepared = false;
+                e.printStackTrace();
+            }
+            return prepared;
+        }
+
+        @Override
+        protected void onPostExecute(Boolean result) {
+            // TODO Auto-generated method stub
+            super.onPostExecute(result);
+            if (progress.isShowing()) {
+                progress.cancel();
+            }
+            Log.d("Prepared", "//" + result);
+            mediaPlayer.start();
+
+            intialStage = false;
+        }
+
+        public Player() {
+            progress = new ProgressDialog(MainActivity.this);
+        }
+
+        @Override
+        protected void onPreExecute() {
+            // TODO Auto-generated method stub
+            super.onPreExecute();
+            this.progress.setMessage("Buffering...");
+            this.progress.show();
+
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        // TODO Auto-generated method stub
+        super.onPause();
+        if (mediaPlayer != null) {
+            mediaPlayer.reset();
+            mediaPlayer.release();
+            mediaPlayer = null;
+        }
+    }
+
+    /*
+    //upload 버튼과 연결된 메소드
+    public void UploadBtnMethod(View v)
+    {
+        String inputURL = edit_url.getText().toString();
+        //사용자가 입력한 url을 받는다.
+
+        String url = "http://........"; // your URL here
+        MediaPlayer mediaPlayer = new MediaPlayer();
+        mediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
+        mediaPlayer.setDataSource(url);
+        mediaPlayer.prepare(); // might take long! (for buffering, etc)
+        mediaPlayer.start();
+
+    }
+    */
+    //end of unpload 버튼과 연결된 메소드
 
     // 버튼과 연결된 메소드
     public void btnMethod(View v) {
@@ -88,7 +247,8 @@ public class MainActivity extends AppCompatActivity {
         public void run() {
             try {
                 // 접속한다.
-                final Socket socket = new Socket("192.168.56.1", 1024);
+                //final Socket socket = new Socket("192.168.56.1", 1024);
+                final Socket socket = new Socket("192.168.35.109", 1024);
                 member_socket=socket;
                 // 미리 입력했던 닉네임을 서버로 전달한다.
                 String nickName = edit1.getText().toString();
@@ -123,7 +283,6 @@ public class MainActivity extends AppCompatActivity {
     //end of ConnectonThread class
 
 
-
     class MessageThread extends Thread {
         Socket socket;
         DataInputStream dis;
@@ -146,6 +305,7 @@ public class MainActivity extends AppCompatActivity {
                     final String msg=dis.readUTF();
                     // 화면에 출력
                     runOnUiThread(new Runnable() {
+
                         @TargetApi(Build.VERSION_CODES.JELLY_BEAN_MR1)
                         @Override
                         public void run() {
